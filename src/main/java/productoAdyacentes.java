5d9e963f7ba793846ac28567feed7a2bcdef086c
LORENZO <l.devia02@ufromail.cl>
2020-05-27 11:02:35 -0400

    Se agregó el método producto adyacentes.

--- src/main/java/productoAdyacentes.java
+++ src/main/java/productoAdyacentes.java
@@ -1,3 +1,16 @@
 public class productoAdyacentes {
-    
+
+    public int productoAdayacentes(int[] arreglo){
+        int m_producto = 0;
+        for (int i = 0; i <= arreglo.length - 2; i++) {
+            if (i == 0) {
+                m_producto = arreglo[i] * arreglo[i + 1];
+            } else {
+                if (m_producto <= arreglo[i] * arreglo[i + 1]) {
+                    m_producto = arreglo[i] * arreglo[i + 1];
+                }
+            }
+        }
+        return m_producto;
+    }
 }
