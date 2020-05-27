1b353747e9f00cebd719b14a4264f3d279a33137
LORENZO <l.devia02@ufromail.cl>
2020-05-27 13:02:02 -0400

    Se agregaron comentarios.

--- src/main/java/productoAdyacentes.java
+++ src/main/java/productoAdyacentes.java
@@ -2,11 +2,23 @@ public class productoAdyacentes {
 
     public int productoAdayacentes(int[] arreglo){
         int m_producto = 0;
+
         for (int i = 0; i <= arreglo.length - 2; i++) {
             if (i == 0) {
+                // (Lorenzo Devia)
+                // Si es el primer par de elementos a comparar,
+                // entonces el primer producto obtenido se le
+                // considerará el mayor y  a partir de este
+                // se irá comparando con los demas
                 m_producto = arreglo[i] * arreglo[i + 1];
+
             } else {
                 if (m_producto <= arreglo[i] * arreglo[i + 1]) {
+                    // (Lorenzo Devia)
+                    // Se compara el actual mayor producto con los
+                    // demás y cada vez que se encuentra un nuevo
+                    // mayor producto se le reemplaza por el
+                    // respectivo
                     m_producto = arreglo[i] * arreglo[i + 1];
                 }
             }
