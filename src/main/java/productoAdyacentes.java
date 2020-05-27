f9311a355982e0c0e118c33bcff84fa198e3adc4
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-05-27 14:08:35 -0400

    Hice cambios en el pom, y se supone que deberían actualizarse las librerías

--- src/main/java/productoAdyacentes.java
+++ src/main/java/productoAdyacentes.java
@@ -1,3 +1,18 @@
 public class productoAdyacentes {
-    
+
+    int[] arreglo = {1, -4, 2, 2, 5, -1};
+
+    public int prodAdyacentes(int[] arreglo){
+        int mayorProducto = -99999;     //valor inicial
+        int producto;
+
+        for (int i = 1; i < arreglo.length; i++){
+            producto = arreglo[i] * arreglo[i - 1];
+            if(producto > mayorProducto){
+                mayorProducto = producto;
+            }
+        }
+
+        return mayorProducto;
+    }
 }
