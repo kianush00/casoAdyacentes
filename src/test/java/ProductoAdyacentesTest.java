ae38d628eb211e805754c5c1821f39699768cacd
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-05-30 16:10:49 -0400

    Ingresé un assertEquals

--- src/test/java/ProductoAdyacentesTest.java
+++ src/test/java/ProductoAdyacentesTest.java
@@ -21,8 +21,9 @@ class ProductoAdyacentesTest {
     @org.junit.jupiter.api.Test
     void productoAdyacentes() {
         int[] arregloPrueba = {1,-4,2,2,5,-1};
-
+        assertEquals(10,a.productoAdyacentes(arregloPrueba));
     }
+
 
 
 }
