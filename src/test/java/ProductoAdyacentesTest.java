852052c410a994769ff29a8b5048fa7f916f1aee
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-05-30 17:24:38 -0400

    cambié el nombre de prodAdyacentes a productoAdyacentes

--- src/test/java/ProductoAdyacentesTest.java
+++ src/test/java/ProductoAdyacentesTest.java
@@ -5,25 +5,47 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductoAdyacentesTest {
-
-    private ProductoAdyacentes a;
-
+    ProductoAdyacentes u;
+    int[] arreglo;
     @BeforeEach
     void setUp() {
-        a = new ProductoAdyacentes();
+        u = new ProductoAdyacentes();
+
     }
 
     @AfterEach
     void tearDown() {
-        a = null;
+        u = null;
     }
 
     @org.junit.jupiter.api.Test
-    void productoAdyacentes() {
-        int[] arregloPrueba = {1,-4,2,2,5,-1};
-        assertEquals(10,a.productoAdyacentes(arregloPrueba));
+    void prodAdyacentes01() {
+        arreglo = new int[] {1, -4, 2, 2, 5, -1};
+        assertEquals(10,u.productoAdyacentes(arreglo));
     }
 
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes02() {
+    }
 
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes03() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes04() {
+        arreglo = new int[] {-1,-1,1,-1,-1,1,-1};
+        assertEquals(1,u.productoAdyacentes(arreglo));
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes05() {
+        arreglo = new int[] {15000,1,2};
+        assertEquals("Error",u.productoAdyacentes(arreglo));
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes06() {
+    }
 
 }
