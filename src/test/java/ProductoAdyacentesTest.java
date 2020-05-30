1c999790ee906a518b3fbcdccd29264d46437e2e
Kianush Atighi-Moghaddam <64434787+kianush00@users.noreply.github.com>
2020-05-30 15:35:15 -0400

    Agregué la clase test de ProductoAdyacentes, viene con setUp, tearDown y productoAdyacentes

---
+++ src/test/java/ProductoAdyacentesTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductoAdyacentesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes() {
+    }
+
+}
