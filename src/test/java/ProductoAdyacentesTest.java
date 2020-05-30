154653539d4cb057f5d607091d2f00f21b775a21
LORENZO <l.devia02@ufromail.cl>
2020-05-30 16:06:14 -0400

    Se agregó el primer caso de prueba.

--- src/test/java/ProductoAdyacentesTest.java
+++ src/test/java/ProductoAdyacentesTest.java
@@ -5,17 +5,43 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductoAdyacentesTest {
-
+    ProductoAdyacentes u;
+    int[] arreglo;
     @BeforeEach
     void setUp() {
+        u = new ProductoAdyacentes();
+
     }
 
     @AfterEach
     void tearDown() {
+        u = null;
     }
 
     @org.junit.jupiter.api.Test
-    void prodAdyacentes() {
+    void prodAdyacentes01() {
+        arreglo = new int[] {1, -4, 2, 2, 5, -1};
+        assertEquals(10,u.prodAdyacentes(arreglo));
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes02() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes03() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes04() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes05() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void prodAdyacentes06() {
     }
 
 }
