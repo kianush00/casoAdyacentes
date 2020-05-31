cd3f2595e9b01d9f03cf38dff2f8a30948ab1468
LORENZO <l.devia02@ufromail.cl>
2020-05-31 12:05:03 -0400

    Se agregó el contenido del método verificadorNoNumeros() para que satisfaciera la respectiva prueba.

--- src/main/java/ProductoAdyacentes.java
+++ src/main/java/ProductoAdyacentes.java
@@ -30,6 +30,21 @@ public class ProductoAdyacentes {
         return 2 <= arreglo.length && arreglo.length <= 20;
     }
     public boolean verificadorNoNumeros(Object[] arreglo){
+        String aux = "";
+
+        System.out.println(aux.length());
+        for (int i = 0; i < arreglo.length; i++) {
+            aux = toString(arreglo[i]);
+            if(aux.length() == 0){
+                return false;
+            }
+            aux = aux.replaceAll("\\d", "");
+            aux = aux.replaceAll(" ", "");
+            aux = aux.replaceAll("-", "");
+            if (aux.length() > 0) {
+                return false;
+            }
+        }
         return true;
     }
     public boolean verificadorValores(Object[] arreglo){
